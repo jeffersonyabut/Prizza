424a92370978ffdd88fe5fe641309b0c483d216a
Jeff <jef.yabut.18@gmail.com.noreply.github.com>
2025-10-05 23:12:09 +0800

    B portfolio

---
+++ script.js
@@ -0,0 +1,108 @@
+const hover_Seffect = new Audio("/public/Effects/tick.mp3");
+const click_Seffect = new Audio("/public/Effects/click.mp3");
+
+function playHoverSound() {
+  const viewportWidth = window.innerWidth;
+  if (viewportWidth < 834) {
+    hover_Seffect.muted = true;
+    hover_Seffect.muted = true;
+    hover_Seffect.volume = 0;
+    click_Seffect.volume = 0;
+
+    document.getElementById("mute-unmute").style.display = "none";
+    document.getElementById("unmute").style.display = "none";
+  }
+}
+
+playHoverSound();
+
+document.querySelectorAll(".card").forEach((el) => {
+  const title = el.getAttribute("data-title");
+  const tech = el.getAttribute("data-tech");
+
+  el.classList.add("box");
+
+  el.innerHTML = `
+        <div class="title-bar" style="height: 26px; border: 0">
+          <h4>${title}</h4>
+          <img src="/public/link.svg" alt="" />
+        </div>
+        <div
+          class="thumbnail"
+          style="width: 314px; height: 148px; align-items: end"
+        >
+        ${tech}
+        </div>
+  `;
+
+  el.addEventListener("mouseenter", () => {
+    click_Seffect.play();
+  });
+
+  el.addEventListener("click", () => {
+    hover_Seffect.play();
+    openLink(title);
+  });
+});
+
+const dl_btn = document.querySelector(".dl_button");
+
+dl_btn.addEventListener("click", () => {
+  hover_Seffect.play();
+});
+
+dl_btn.addEventListener("mouseenter", () => {
+  click_Seffect.play();
+});
+
+document.querySelectorAll(".nav-btn").forEach((el) => {
+  el.addEventListener("mouseenter", () => {
+    click_Seffect.play();
+  });
+
+  el.addEventListener("click", () => {
+    hover_Seffect.play();
+  });
+});
+
+function openLink(title) {
+  if (title === "Portfolio") {
+    window.open("https://youtube.com", "_blank");
+  }
+  if (title === "Prizzabooth") {
+    window.open("https://facebook.com", "_blank");
+  }
+}
+
+function scrollProject() {
+  document
+    .getElementById("project-container")
+    .scrollIntoView({ behavior: "smooth" });
+}
+
+function downloadCV(filePath) {
+  const link = document.createElement("a");
+  link.href = filePath;
+  link.download = "jefferson_cv.pdf";
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function unmute(value) {
+  if (value) {
+    document.getElementById("mute-unmute").style.display = "none";
+    document.getElementById("unmute").style.display = "flex";
+
+    hover_Seffect.volume = 1;
+    click_Seffect.volume = 1;
+    click_Seffect.play();
+  }
+
+  if (!value) {
+    document.getElementById("unmute").style.display = "none";
+    document.getElementById("mute-unmute").style.display = "flex";
+    hover_Seffect.volume = 0;
+    click_Seffect.volume = 0;
+  }
+}
