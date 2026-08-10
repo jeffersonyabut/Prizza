0c684d6e21383a5bbeaf91eb4b102880f479a783
Jeff <jef.yabut.18@gmail.com.noreply.github.com>
2026-08-11 06:50:09 +0800

    link path

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ document.querySelectorAll(".card").forEach((el) => {
 
   el.innerHTML = `
         <div style="width: 100%; height: 16px; display: flex; justify-content: flex-end; ">
-        <img src="/public/link.svg" alt="link" />
+
+        <img src="/prizza/public/link.svg" alt="link" />
         </div>      
         <div class="title-bar">
           <h4>${title}</h4>
